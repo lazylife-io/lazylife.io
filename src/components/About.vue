d7b17835b31b8f6f796e337500623031f2c2a007
lazylife7157 <lazylife7157@gmail.com>
2017-11-26 02:37:55 +0900

    Simple main page

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <img class="logo" src="../assets/sloth.png" alt="sloth" />
+    <h1>Welcome to lazylife</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style lang="scss">
+section {
+  margin-top: 8rem;
+  padding: 1rem;
+  img.logo {
+    padding: 1rem;
+    width: 16rem;
+    height: 16rem;
+  }
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="item in items">
+        <a :href="item.href">{{item.text}}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+let items = [
+  {
+    text: 'lazylife',
+    href: '#'
+  }
+]
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      items: items
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  margin: 0;
+  padding: 1rem;
+  border-bottom: solid #222 0.4rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    text-align: left;
+    li {
+      display: inline-block;
+      padding: 0 0.8rem;
+      vertical-align: middle;
+      a {
+        outline: none;
+        text-decoration: none;
+        color: #222;
+        font-size: 1.25rem;
+      }
+      a:hover {
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
